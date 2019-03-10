cd96290e500c5fe7ab34f148e0730d29d89c6e23
741boss741 <47608727+741boss741@users.noreply.github.com>
2019-03-10 04:50:40 +0100

    Update shard.js

--- shard.js
+++ shard.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on('message', message => {
-   let emoji = client.guilds.get("511225819415183360").emojis.find(r => r.name === "right"); //كود تعريف المتغير emoji
+   let emoji = client.guilds.get("554134139226292244").emojis.find(r => r.name === "emoji_12"); //كود تعريف المتغير emoji
  
     if(message.content === 'sendemoji'){ //تحقق اذا الرساله هي sebdemji
    message.channel.send(`${emoji}`)//يرسل المتغير emoji 
@@ -14,21 +14,13 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-   let emoji = client.guilds.get("511225819415183360").emojis.find(r => r.name === "left"); //كود تعريف المتغير emoji
+   let emoji = client.guilds.get("554134139226292244").emojis.find(r => r.name === "emoji_11"); //كود تعريف المتغير emoji
  
     if(message.content === 'sendemoji'){ //تحقق اذا الرساله هي sebdemji
    message.channel.send(`${emoji}`)//يرسل المتغير emoji 
     }  // xomar933 نهاية قوس التحقق
 }); // x نهاية قوس الايفنت 
 
-
-client.on('message', message => {
-   let emoji = client.guilds.get("511225819415183360").emojis.find(r => r.name === "rly"); //كود تعريف المتغير emoji
- 
-    if(message.content === 'sendemoji'){ //تحقق اذا الرساله هي sebdemji
-   message.channel.send(`${emoji}`)//يرسل المتغير emoji 
-    }  // xomar933 نهاية قوس التحقق
-}); // x نهاية قوس الايفنت 
 
 client.on('message', message => {
    let emoji = client.guilds.get("554134139226292244").emojis.find(r => r.name === "emoji_1"); //كود تعريف المتغير emoji
